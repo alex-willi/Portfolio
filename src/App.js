55e155e28389f00253cb7ca5128becad33f77165
alex-willi <alex.lawson.williams@gmail.com>
2022-12-12 11:07:46 -0600

    frontend complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// IMPORT COMPONENTS
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+// IMPORT PAGES
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 function App() {
+  // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
+  const URL = "http://localhost:4000/";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects URL={URL} />} />
+        <Route path="/about" element={<About URL={URL} />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,32 @@
+import { Link } from "react-router-dom";
+
 function Header(props) {
-  return <h1>Header</h1>;
+  //inline style for the nav tag
+  const navStyle = {
+    display: "flex",
+    justifyContent: "space-around",
+    border: "3px solid black",
+    padding: "8px",
+    width: "90%",
+    margin: "auto",
+  };
+
+  return (
+    <header>
+      <h1>My Portfolio Page</h1>
+      <nav style={navStyle}>
+        <Link to="/">
+          <div>HOME</div>
+        </Link>
+        <Link to="/about">
+          <div>ABOUT</div>
+        </Link>
+        <Link to="/projects">
+          <div>PROJECTS</div>
+        </Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
